dfa0c1eee3516a3362191397a5ae5cf3f7878da5
anooppv567 <anoopit2010@gmail.com>
2020-07-19 19:01:31 -0500

    4 files modified

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit,Input ,Output, EventEmitter} from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 @Input () userNameFromParent ='' ;
+@Input() phoneNumberFromParent = '';
  @Output () nameEvent = new EventEmitter<string>();
  userName='';
   constructor() { }
